a7c8d2c890fbfcb56e033c1e85e08384a7395777
KenTyler1 <tonykhanh999@gmail.com>
2022-12-04 14:47:48 +0700

    fix-forgot-password

--- server/app.js
+++ server/app.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.set("view engine", "ejs");
-
+app.use(express.static(__dirname + '/public'));
 //Body-parser
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
